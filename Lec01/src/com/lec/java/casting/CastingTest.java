231f30a29a3ec29100c45031da307e3931d17ff1
binna <every5116@naver.com>
2020-08-14 13:32:24 +0900

    2020.08.11

--- Lec01/src/com/lec/java/casting/CastingTest.java
+++ Lec01/src/com/lec/java/casting/CastingTest.java
@@ -1,5 +1,13 @@
 package com.lec.java.casting;
 
-public class CastingTest {
-
+class Car {
+	String brand;
+	String color;
+	int price;
+	
+	// 기본생성자 만드는 단축키 : Ctrl + SpaceBar
+	public Car() {}
+	
 }
+
+public class CastingTest {}
