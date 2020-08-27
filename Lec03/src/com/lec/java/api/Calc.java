5a981474843a9f06bb07caaa37efc48d6e619235
binna <every5116@naver.com>
2020-08-28 02:31:06 +0900

    2020.08.27

---
+++ Lec03/src/com/lec/java/api/Calc.java
@@ -0,0 +1,25 @@
+package com.lec.java.api;
+/**
+ * 
+ * @author ¹Úºó³ª
+ * @since JDK8
+ * <br>Calculator 
+ */
+public class Calc {
+	
+	/**
+	 * To divide the following integers.<br>
+	 * For example<br>
+	 * <code>div(10, 3) : 3</code>
+	 * 
+	 * @param num1 : First Integer for divide
+	 * @param num2 : Last Integer for divide
+	 * @see Math
+	 * @return int
+	 * @throws ArithmeticException
+	 */
+	public int div(int num1, int num2) {
+		return num1 / num2;
+	}
+	
+}
