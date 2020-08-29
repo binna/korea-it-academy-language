f4e943226fc44e748dff4feb9d31b161a8a2dab8
binna <every5116@naver.com>
2020-08-29 20:21:46 +0900

    2020.08.27

---
+++ Lec03/src/com/lec/java/wrapperClass/WrapperClass.java
@@ -0,0 +1,30 @@
+package com.lec.java.wrapperClass;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		int data = 10;
+		
+		// boxing
+		Integer data_I = new Integer(data);
+		System.out.println(data_I);
+		
+		// unboxing
+		data = data_I.intValue();
+		System.out.println(data);
+		
+		// 위의 같은 방식은 너무 너무 너무 번거롭다
+		// 번거로움을 극복하기 위해 JDK 5 버전 이후부터는 오토(auto)를 지원한다.
+		int data_auto= 20;
+		
+		// auto boxing
+		Integer data_auto_I = data_auto;
+		System.out.println(data_auto_I);
+		
+		// auto unboxing
+		data_auto = data_auto_I;
+		System.out.println(data_auto);
+		
+	} // end main()
+	
+} // end class
---
+++ Lec03/src/com/lec/java/wrapperClass/WrapperClass2.java
@@ -0,0 +1,32 @@
+package com.lec.java.wrapperClass;
+
+public class WrapperClass2 {
+	
+	public static void main(String[] args) {
+		// 다른 타입은 배열로 담을 수 없다.
+		// 그러나 아래의 방법을 이용하면 담을 수 있다.
+		Object[] datas = {3, 3.0, 'A', "ABC", true};
+		
+		// 위의 datas들에서 사용된 기술
+		// 1. up-casting
+		// 2. auto-boxing
+		
+		// auto-boxing 사용하지 않는다면...!!
+		Object[] datasNoAuto = {new Integer(3), new Double(3.0),new Character('A'), new String("ABC"), new Boolean(true)};
+		
+		// 빠른 for문(forEach문, 향상된 for문)을 이용하여 저장된 데이터 출력하기
+		System.out.println("빠른 for문 : datas");
+		for(Object obj : datas) {
+			System.out.print(obj + " ");
+		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("빠른 for문 : datasNoAuto");
+		for(Object obj : datasNoAuto) {
+			System.out.print(obj + " ");
+		}
+		
+	} // end main()
+
+} // end class
