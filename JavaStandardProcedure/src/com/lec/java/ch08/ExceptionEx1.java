0657441ce121c01d120ad2d2c5caaf353fd97160
binna <every5116@naver.com>
2020-08-30 00:23:14 +0900

    자바의정석

---
+++ JavaStandardProcedure/src/com/lec/java/ch08/ExceptionEx1.java
@@ -0,0 +1,22 @@
+package com.lec.java.ch08;
+
+public class ExceptionEx1 {
+	
+	public static void main(String[] args) {
+		
+		try {
+			try {} catch (Exception e) {}
+		} catch (Exception e) {
+			//try {} catch (Exception e) {}	// 에러 : Duplicate parameter e
+		} // end try-catch()
+		
+		try {
+			
+		} catch (Exception e) {
+			
+		} // end try-catch()
+		
+		System.out.println("프로그램 종료");
+	} // end main()
+
+} // end class
