33c4d5598b424e8f54a720bb66e32c02d9321fc3
binna <every5116@naver.com>
2020-08-29 21:34:30 +0900

    2020.08.27

---
+++ Lec03/src/com/lec/java/forEach/ForEachTest.java
@@ -0,0 +1,31 @@
+package com.lec.java.forEach;
+
+public class ForEachTest {
+
+	public static void main(String[] args) {
+		// 2차원 배열을 빠른 for문 사용
+		// 총점과 평균을 구하기
+		int[][] arrScore = {
+				{10, 20, 30},
+				{11, 25, 39},
+				{12, 26, 38},
+				{13, 27, 37},
+				{14, 28, 36}
+		};
+		
+		double avg = 0.0;
+		
+		for(int[] arScore : arrScore) {
+			int total = 0;
+			for(int score : arScore) {
+				total += score;
+			}
+			avg = (double)total / arScore.length;
+			System.out.println("총점 : " + total + "점");
+			System.out.println("평균 : " + avg + "점");
+			System.out.println("===================");
+		}
+		
+	} // main()
+	
+} // class
